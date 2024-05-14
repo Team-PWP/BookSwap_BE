b2fa7585c7c0e0e769088553f70db6a4cae3fb53
momnpa333 <momnpa333@gmail.com>
2024-05-14 14:59:44 +0900

    Init(): 프로젝트 init

---
+++ src/main/java/team_pwp/swap_be/SwapBeApplication.java
@@ -0,0 +1,13 @@
+package team_pwp.swap_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwapBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team_pwp/swap_be/SwapBeApplicationTests.java
@@ -0,0 +1,13 @@
+package team_pwp.swap_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
