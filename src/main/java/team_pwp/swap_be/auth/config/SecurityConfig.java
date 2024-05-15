77a6645db5e24a895f984f892bab2c36e38694b4
momnpa333 <momnpa333@gmail.com>
2024-05-15 23:21:06 +0900

    feat(Security): SecurityConfig 및 service 제작
     #2

---
+++ src/main/java/team_pwp/swap_be/auth/config/SecurityConfig.java
@@ -0,0 +1,77 @@
+package team_pwp.swap_be.auth.config;
+
+import java.util.Arrays;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import team_pwp.swap_be.auth.error.CustomAccessDeniedHandler;
+import team_pwp.swap_be.auth.error.CustomAuthenticationEntryPoint;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@Slf4j
+public class SecurityConfig {
+
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
+    private final JwtAuthorizationFilter jwtAuthorizationFilter;
+
+    private static final String[] WHITE_LIST = {
+        "/auth2",
+        "/api-docs/**",
+        "/login/**",
+    };
+    private static final String[] AUTHENTICATION_LIST = {
+        "/api/gifticon"
+    };
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        log.info("필터체인 실행");
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .cors(c -> c.configurationSource(corsConfigSource()))
+            .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(authReq -> authReq
+                .requestMatchers(HttpMethod.OPTIONS).permitAll()
+                .requestMatchers("/", "/login", "/oauth2/**", "/refresh", "/swagger-ui/**",
+                    "/api-docs", "/v3/api-docs/**").permitAll()
+                .anyRequest().authenticated())
+            .exceptionHandling(e -> e
+                .authenticationEntryPoint(customAuthenticationEntryPoint)
+                .accessDeniedHandler(customAccessDeniedHandler))
+            .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));    // 모든 요청 허용
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH",
+            "OPTIONS"));      // 모든 HTTP 메서드 허용
+        configuration.setAllowedHeaders(Arrays.asList("*"));    // 모든 헤더 허용
+        configuration.setExposedHeaders(Arrays.asList("Authorization", "Set-cookie"));
+        configuration.setAllowCredentials(true);    // 쿠키와 같은 자격 증명을 허용
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+}
