2defbf6e4809ccf7b02408f2d585b54fd8e6204e
momnpa333 <momnpa333@gmail.com>
2024-05-16 21:34:59 +0900

    feat(article): article 엔티티 제작
     #9

---
+++ src/main/java/team_pwp/swap_be/entity/article/Article.java
@@ -0,0 +1,67 @@
+package team_pwp.swap_be.entity.article;
+
+import static lombok.AccessLevel.PROTECTED;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import jdk.jfr.Name;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import team_pwp.swap_be.entity.user.User;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = PROTECTED)
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    private String content;
+
+    @NotNull
+    private Long buyoutPrice;
+
+    @NotNull
+    private Long minPrice;
+
+    @NotNull
+    private LocalDateTime createdAt;
+
+    @NotNull
+    private LocalDateTime bidStartAt;
+
+    @NotNull
+    private LocalDateTime bidEndAt;
+
+    @Builder
+    private Article(User user, String title, String content, Long buyoutPrice, Long minPrice,
+        LocalDateTime createdAt, LocalDateTime bidStartAt, LocalDateTime bidEndAt) {
+        this.user = user;
+        this.title = title;
+        this.content = content;
+        this.buyoutPrice = buyoutPrice;
+        this.minPrice = minPrice;
+        this.createdAt = createdAt;
+        this.bidStartAt = bidStartAt;
+        this.bidEndAt = bidEndAt;
+    }
+}
