3b887479bb71976b1157c3834a8b6e6efe69d123
momnpa333 <momnpa333@gmail.com>
2024-05-17 20:50:10 +0900

    feat(article): article 권한 관련 설정
     #9

--- src/main/java/team_pwp/swap_be/auth/config/SecurityConfig.java
+++ src/main/java/team_pwp/swap_be/auth/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
             .authorizeHttpRequests(authReq -> authReq
                 .requestMatchers(HttpMethod.OPTIONS).permitAll()
                 .requestMatchers("/", "/login", "/oauth2/**", "/refresh", "/swagger-ui/**",
-                    "/api-docs", "/v3/api-docs/**", "api/article/**").permitAll()
+                    "/api-docs", "/v3/api-docs/**").permitAll()
                 .anyRequest().authenticated())
             .exceptionHandling(e -> e
                 .authenticationEntryPoint(customAuthenticationEntryPoint)
