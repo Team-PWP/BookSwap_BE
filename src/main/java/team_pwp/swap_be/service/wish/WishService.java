9088ea9ee932b67af3a06cb15c7176330e434474
momnpa333 <momnpa333@gmail.com>
2024-05-24 18:26:56 +0900

    feat(wish): 찜 목록 조회 수정 및 찜 삭제 기능 추가
    #26

--- src/main/java/team_pwp/swap_be/service/wish/WishService.java
+++ src/main/java/team_pwp/swap_be/service/wish/WishService.java
@@ -7,10 +7,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team_pwp.swap_be.domain.article.ArticleImage;
+import team_pwp.swap_be.domain.wish.WishArticleImage;
 import team_pwp.swap_be.dto.article.response.ArticleInfoResponse;
 import team_pwp.swap_be.dto.article.response.ArticleResponse;
 import team_pwp.swap_be.dto.common.PagingRequest;
 import team_pwp.swap_be.dto.common.PagingResponse;
+import team_pwp.swap_be.dto.wish.response.WishResponse;
 import team_pwp.swap_be.entity.article.Article;
 import team_pwp.swap_be.entity.user.User;
 import team_pwp.swap_be.entity.wish.Wish;
@@ -49,11 +51,33 @@ public class WishService {
         return wish.getId();
     }
 
-    public PagingResponse<ArticleResponse> getWishList(PagingRequest pagingRequest, Long userId) {
+    /**
+     * 찜 목록 조회
+     */
+    public PagingResponse<WishResponse> getWishList(PagingRequest pagingRequest, Long userId) {
         log.info("찜 목록 조회");
 
-        Page<ArticleImage> articleImages = wishJpaRepository.findAllByUserId(userId,
+        User user = userJpaRepository.findById(userId)
+            .orElseThrow(() -> new IllegalArgumentException("유저가 존재하지 않습니다."));
+        Page<WishArticleImage> wishArticleImages = wishJpaRepository.findAllByUserId(userId,
             pagingRequest.toPageable());
-        return PagingResponse.from(articleImages, ArticleResponse::from);
+        return PagingResponse.from(wishArticleImages, WishResponse::from);
+    }
+
+    /**
+     * 찜 취소
+     */
+    public void deleteWish(Long wishId, Long userId) {
+        log.info("찜 취소 하기");
+        Wish wish = wishJpaRepository.findById(wishId)
+            .orElseThrow(() -> new IllegalArgumentException("찜이 존재하지 않습니다."));
+        User user = userJpaRepository.findById(userId)
+            .orElseThrow(() -> new IllegalArgumentException("유저가 존재하지 않습니다."));
+        // 찜한 게시글이 본인이 찜한 게시글인지 확인
+        if (!wish.getUser().equals(user)) {
+            throw new IllegalArgumentException("본인이 찜한 게시글이 아닙니다.");
+        }
+        // 찜 취소
+        wishJpaRepository.delete(wish);
     }
 }
