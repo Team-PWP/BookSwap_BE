85827b85642a721944119eaddaa020bfa6cc9c67
momnpa333 <momnpa333@gmail.com>
2024-05-24 16:36:33 +0900

    feat(bid): bid 관련 api 제작
    #23

--- src/main/java/team_pwp/swap_be/service/bid/BidService.java
+++ src/main/java/team_pwp/swap_be/service/bid/BidService.java
@@ -2,11 +2,14 @@ package team_pwp.swap_be.service.bid;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team_pwp.swap_be.domain.Bid.BidCreate;
 import team_pwp.swap_be.dto.bid.request.BidRequest;
 import team_pwp.swap_be.dto.bid.response.BidResponse;
+import team_pwp.swap_be.dto.common.PagingRequest;
+import team_pwp.swap_be.dto.common.PagingResponse;
 import team_pwp.swap_be.entity.article.Article;
 import team_pwp.swap_be.entity.bid.Bid;
 import team_pwp.swap_be.entity.user.User;
@@ -64,9 +67,48 @@ public class BidService {
             throw new IllegalArgumentException("입찰 기간이 종료된 게시글입니다.");
         }
 
+        /**
+         * 이전 입찰 기록 제거
+         */
+        bidJpaRepository.deleteByArticleIdAndUserId(bidCreate.getArticleId(), userId);
+
         Bid bid = Bid.from(bidCreate, article, user);
         bidJpaRepository.save(bid);
 
         return BidResponse.from(bid, user.getNickname());
+    }
+
+
+    /**
+     * 입찰 취소 하기
+     */
+    public void cancelBid(Long bidId, Long userId) {
+        log.info("입찰 취소 하기");
+
+        Bid bid = bidJpaRepository.findById(bidId)
+            .orElseThrow(() -> new IllegalArgumentException("입찰 정보 조회 실패"));
+
+        if (!bid.getUser().getId().equals(userId)) {
+            throw new IllegalArgumentException("입찰자만 입찰을 취소할 수 있습니다.");
+        }
+
+        bidJpaRepository.deleteById(bidId);
+    }
+
+    /**
+     * 게시글 입찰 내역 조회
+     */
+    public PagingResponse<BidResponse> getBidsByArticleId(PagingRequest pagingRequest,
+        Long articleId) {
+        Page<Bid> bids = bidJpaRepository.findAllByArticleId(articleId, pagingRequest.toPageable());
+        return PagingResponse.from(bids, bid -> BidResponse.from(bid, bid.getUser().getNickname()));
+    }
+
+    /**
+     * 유저별 입찰 목록 조회
+     */
+    public PagingResponse<BidResponse> getBidsByUser(PagingRequest pagingRequest, long userId) {
+        Page<Bid> bids = bidJpaRepository.findAllByUserId(userId, pagingRequest.toPageable());
+        return PagingResponse.from(bids, bid -> BidResponse.from(bid, bid.getArticle().getTitle()));
     }
 }
